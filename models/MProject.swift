f840a5910c2bee2762aebe0d99f1cb3a990782f1
Miguel Moldes <jmoldes@ring.com>
2021-09-19 11:05:27 -0300

    MProjectHelper

--- models/MProject.swift
+++ models/MProject.swift
@@ -8,6 +8,12 @@
 import Foundation
 import UIKit
 
+// definir cómo guardar las tareas que no tengan dependencias
+// when removing tasks, -> remove relationships
+// when removing relationships -> recreate connections
+// edit tasks? maybe recreate them? how to keep relationships?
+// make everything Codable and save it locally
+
 public struct MProject {
     public let id: Int
     public let name: String
@@ -22,25 +28,23 @@ public struct MProject {
         self.name = name
         self.startDate = startDate
     }
+}
 
-    public mutating func addTask(_ task: MTask) throws {
-        try canAddTask(task)
+fileprivate extension MProject {
+    mutating func addTask(_ task: MTask) {
         tasks.append(task)
     }
 
-    public mutating func removeTask(id: MTask.Id) {
+    mutating func removeTask(id: MTask.Id) {
         tasks.removeAll(where: { $0.id == id })
-        // remove relationships
     }
 
-    public mutating func addRelationship(_ relationship: Relationship) throws {
-        try canAddRelationship(relationship)
+    mutating func addRelationship(_ relationship: Relationship) {
         relationships.append(relationship)
     }
 
-    public mutating func removeRelationship(_ id: Relationship.Id) {
+    mutating func removeRelationship(_ id: Relationship.Id) {
         relationships.removeAll(where: { $0.id == id} )
-        // recreate connections
     }
 
 }
@@ -101,12 +105,19 @@ public enum Palette {
     case red, yellow, blue, orange, grey, green
 }
 
-public extension MProject {
-    func tasksByDays() -> [UInt: [MTask]] {
+public protocol MProjectHelperProtocol {
+    func tasksByDays(_ project: MProject) -> [UInt: [MTask]]
+    func addTask(_ project: inout MProject, _ task: MTask) throws
+    func removeTask(_ project: inout MProject, taskId: MTask.Id)
+    func addRelationship(_ project: inout MProject, _ relationship: Relationship) throws
+    func removeRelationship(_ project: inout MProject, _ relationshipId: Relationship.Id)
+}
+public class MProjectHelper: MProjectHelperProtocol {
+    public func tasksByDays(_ project: MProject) -> [UInt: [MTask]] {
         var dic: [UInt: [MTask]] = [UInt: [MTask]]()
         dic[0] = [MTask]()
-        self.tasks.forEach { task in
-            let days = UInt(getDays(task: task))
+        project.tasks.forEach { task in
+            let days = UInt(getDays(project, task: task))
             if dic.index(forKey: days) == nil {
                 dic[days] = [MTask]()
             }
@@ -114,66 +125,87 @@ public extension MProject {
         }
         return dic
     }
-}
 
-private extension MProject {
-
-    private func getDays(task: MTask) -> Int {
-        guard let relationship = relationships.first(where: { $0.dependant.id == task.id })  else {
-            return 0
-        }
-        return getDays(task: relationship.influencer) + Int(relationship.influencer.days) + relationship.daysGap
+    public func addTask(_ project: inout MProject, _ task: MTask) throws {
+        try canAddTask(project, task)
+        project.addTask(task)
     }
 
-    private func canAddTask(_ task: MTask) throws {
-        if tasks.contains(task) { throw MEditingProjectError.taskAlreadyExists }
-        if tasks.first(where: { $0.id == task.id }) != nil { throw MEditingProjectError.taskIdRepeated }
+    public func removeTask(_ project: inout MProject, taskId: MTask.Id) {
+        project.removeTask(id: taskId)
+        // remove relationships
+    }
+
+    public func addRelationship(_ project: inout MProject, _ relationship: Relationship) throws {
+        try canAddRelationship(project, relationship)
+        project.addRelationship(relationship)
+    }
+
+    public func removeRelationship(_ project: inout MProject, _ relationshipId: Relationship.Id) {
+        project.removeRelationship(relationshipId)
+        // recreate connections
+    }
+
+}
+
+private extension MProjectHelper {
+
+    private func getDays(_ project: MProject, task: MTask) -> Int {
+        guard let relationship = project.relationships.first(where: { $0.dependant.id == task.id })  else {
+            return 0
+        }
+        return getDays(project, task: relationship.influencer) + Int(relationship.influencer.days) + relationship.daysGap
+    }
+
+    private func canAddTask(_ project: MProject, _ task: MTask) throws {
+        if project.tasks.contains(task) { throw MEditingProjectError.taskAlreadyExists }
+        if project.tasks.first(where: { $0.id == task.id }) != nil { throw MEditingProjectError.taskIdRepeated }
         if task.days <= 0 { throw MEditingProjectError.daysBiggerThanZero }
     }
 
-    private func canAddRelationship(_ relationship: Relationship) throws {
-        if relationships.contains(relationship) { throw MEditingProjectError.relationshipAlreadyExists }
-        if relationships.first(where: { $0.id == relationship.id }) != nil { throw MEditingProjectError.relationshipIdRepeated }
+    private func canAddRelationship(_ project: MProject, _ relationship: Relationship) throws {
+        if project.relationships.contains(relationship) { throw MEditingProjectError.relationshipAlreadyExists }
+        if project.relationships.first(where: { $0.id == relationship.id }) != nil { throw MEditingProjectError.relationshipIdRepeated }
         let t1 = relationship.influencer
         let t2 = relationship.dependant
         var unexistingTasks: [MTask.Id] = [MTask.Id]()
-        if tasks.contains(t1) == false { unexistingTasks.append(t1.id) }
-        if tasks.contains(t2) == false { unexistingTasks.append(t2.id) }
+        if project.tasks.contains(t1) == false { unexistingTasks.append(t1.id) }
+        if project.tasks.contains(t2) == false { unexistingTasks.append(t2.id) }
         if unexistingTasks.isEmpty == false {
             throw MEditingProjectError.unexistingTasks(unexistingTasks)
         }
         // there shouldn't be a cycle reference
-        if influencerDependsOnDependant(influencer: t1, dependant: t2) {
+        if influencerDependsOnDependant(project, influencer: t1, dependant: t2) {
             throw MEditingProjectError.cycleReference
         }
         // it already depends indirectly
-        if dependsIndirectly(influencer: t1, dependant: t2) {
+        if dependsIndirectly(project, influencer: t1, dependant: t2) {
             throw MEditingProjectError.taskAlreadyDependsOnInfluencerIndirectly
         }
     }
 
-    private func influencerDependsOnDependant(influencer: MTask, dependant: MTask) -> Bool {
-        let filtered = self.relationships.filter { $0.dependant.id == influencer.id }
+    private func influencerDependsOnDependant(_ project: MProject, influencer: MTask, dependant: MTask) -> Bool {
+        let filtered = project.relationships.filter { $0.dependant.id == influencer.id }
         guard filtered.count > 0 else { return false }
         for relationship in filtered {
             if relationship.influencer.id == dependant.id {
                 return true
             }
-            if influencerDependsOnDependant(influencer: relationship.influencer, dependant: dependant) {
+            if influencerDependsOnDependant(project, influencer: relationship.influencer, dependant: dependant) {
                 return true
             }
         }
         return false
     }
 
-    private func dependsIndirectly(influencer: MTask, dependant: MTask) -> Bool {
-        let filtered = self.relationships.filter { $0.dependant.id == dependant.id }
+    private func dependsIndirectly(_ project: MProject, influencer: MTask, dependant: MTask) -> Bool {
+        let filtered = project.relationships.filter { $0.dependant.id == dependant.id }
         guard filtered.count > 0 else { return false }
         for relationship in filtered {
             if relationship.influencer.id == influencer.id {
                 return true
             }
-            if dependsIndirectly(influencer: influencer, dependant: relationship.influencer) {
+            if dependsIndirectly(project, influencer: influencer, dependant: relationship.influencer) {
                 return true
             }
         }
--- modelsTests/MProjectHelperTests.swift
+++ modelsTests/MProjectHelperTests.swift
@@ -1,52 +1,55 @@
 //
-//  modelsTests.swift
-//  modelsTests
+//  MProjectHelperTests.swift
+//  MProjectHelperTests
 //
-//  Created by Moldes, Jose on 22/08/2021.
+//  Created by Moldes, Miguel on 22/08/2021.
 //
 
 import XCTest
 
 @testable import models
-class modelsTests: XCTestCase {
+class MProjectHelperTests: XCTestCase {
 
     func testAddAndRemoveTasks() throws {
-        var project = createSUT()
+        let sut = createSUT()
+        var project = createProject()
         let task1 = createTask(id: MTask.Id(1))
         let task2 = createTask(id: MTask.Id(2))
         let task3 = createTask(id: MTask.Id(3))
-        try project.addTask(task1)
-        try project.addTask(task2)
-        try project.addTask(task3)
+        try sut.addTask(&project, task1)
+        try sut.addTask(&project, task2)
+        try sut.addTask(&project, task3)
         XCTAssertEqual(project.tasks.count, 3)
         XCTAssertEqual(project.tasks, [task1, task2, task3])
 
-        project.removeTask(id: task2.id)
+        sut.removeTask(&project, taskId: task2.id)
         XCTAssertEqual(project.tasks.count, 2)
         XCTAssertEqual(project.tasks, [task1, task3])
     }
 
     func testAddintExistingTask_Error() throws {
-        var project = createSUT()
+        let sut = createSUT()
+        var project = createProject()
         let task1 = createTask(id: MTask.Id(1),
                                name: "Plot cleaning")
         let taskWithSameId = createTask(id: MTask.Id(1))
         let taskWithDaysLessThanOne = createTask(id: MTask.Id(3),
                                                  days: 0)
-        try project.addTask(task1)
-        XCTAssertThrowsError(try project.addTask(taskWithSameId)) { error in
+        try sut.addTask(&project, task1)
+        XCTAssertThrowsError(try sut.addTask(&project, taskWithSameId)) { error in
             XCTAssertEqual(error as? MEditingProjectError, MEditingProjectError.taskIdRepeated)
         }
-        XCTAssertThrowsError(try project.addTask(task1)) { error in
+        XCTAssertThrowsError(try sut.addTask(&project, task1)) { error in
             XCTAssertEqual(error as? MEditingProjectError, .taskAlreadyExists)
         }
-        XCTAssertThrowsError(try project.addTask(taskWithDaysLessThanOne)) { error in
+        XCTAssertThrowsError(try sut.addTask(&project, taskWithDaysLessThanOne)) { error in
             XCTAssertEqual(error as? MEditingProjectError, .daysBiggerThanZero)
         }
     }
 
     func testAddAndRemoveRelationships() throws {
-        var sut = createSUT()
+        let sut = createSUT()
+        var project = createProject()
         let influencer = createTask(id: MTask.Id(1), days: 5)
         let dependant = createTask(id: MTask.Id(2), days: 3)
 
@@ -54,16 +57,17 @@ class modelsTests: XCTestCase {
                                          influencer: influencer,
                                          dependant: dependant,
                                          daysGap: 0)
-        try sut.addTask(influencer)
-        try sut.addTask(dependant)
-        try sut.addRelationship(relationship)
-        XCTAssertEqual(sut.relationships, [relationship])
-        sut.removeRelationship(relationship.id)
-        XCTAssertEqual(sut.relationships, [])
+        try sut.addTask(&project, influencer)
+        try sut.addTask(&project, dependant)
+        try sut.addRelationship(&project, relationship)
+        XCTAssertEqual(project.relationships, [relationship])
+        sut.removeRelationship(&project, relationship.id)
+        XCTAssertEqual(project.relationships, [])
     }
 
     func testAddingRelationships_Errors() throws {
-        var sut = createSUT()
+        let sut = createSUT()
+        var project = createProject()
         let influencer = createTask(id: MTask.Id(1), days: 5)
         let dependant = createTask(id: MTask.Id(2), days: 3)
 
@@ -71,18 +75,18 @@ class modelsTests: XCTestCase {
                                          influencer: influencer,
                                          dependant: dependant,
                                          daysGap: 0)
-        XCTAssertThrowsError(try sut.addRelationship(relationship)) { error in
+        XCTAssertThrowsError(try sut.addRelationship(&project, relationship)) { error in
             XCTAssertEqual(error as? MEditingProjectError, .unexistingTasks([influencer.id, dependant.id]))
         }
 
         // adding tasks
-        try sut.addTask(influencer)
-        try sut.addTask(dependant)
+        try sut.addTask(&project, influencer)
+        try sut.addTask(&project, dependant)
 
-        try sut.addRelationship(relationship)
+        try sut.addRelationship(&project, relationship)
 
         // trying to add the same relationship
-        XCTAssertThrowsError(try sut.addRelationship(relationship)) { error in
+        XCTAssertThrowsError(try sut.addRelationship(&project, relationship)) { error in
             XCTAssertEqual(error as? MEditingProjectError, .relationshipAlreadyExists)
         }
 
@@ -93,13 +97,14 @@ class modelsTests: XCTestCase {
                                          influencer: task3,
                                          dependant: task4,
                                          daysGap: 0)
-        XCTAssertThrowsError(try sut.addRelationship(relationship2)) { error in
+        XCTAssertThrowsError(try sut.addRelationship(&project, relationship2)) { error in
             XCTAssertEqual(error as? MEditingProjectError, .relationshipIdRepeated)
         }
     }
 
     func testTasksPositions() throws {
-        var sut = createSUT()
+        let sut = createSUT()
+        var project = createProject()
         let task1 = createTask(id: .init(1))
         let task2 = createTask(id: .init(2))
         let task3 = createTask(id: .init(3))
@@ -108,13 +113,13 @@ class modelsTests: XCTestCase {
         let task6 = createTask(id: .init(6))
 
         // no tasks added, no tasks to show
-        XCTAssertEqual(sut.tasksByDays()[0], [])
-        try sut.addTask(task1)
-        try sut.addTask(task2)
-        try sut.addTask(task3)
-        try sut.addTask(task4)
-        try sut.addTask(task5)
-        try sut.addTask(task6)
+        XCTAssertEqual(sut.tasksByDays(project)[0], [])
+        try sut.addTask(&project, task1)
+        try sut.addTask(&project, task2)
+        try sut.addTask(&project, task3)
+        try sut.addTask(&project, task4)
+        try sut.addTask(&project, task5)
+        try sut.addTask(&project, task6)
 
         let relationship1 = Relationship(id: .init(1),
                                         influencer: task1,
@@ -126,27 +131,28 @@ class modelsTests: XCTestCase {
                                         daysGap: -2)
 
         // no relationships added, all tasks start from day 1
-        XCTAssertEqual(sut.tasksByDays()[0], [task1, task2, task3, task4, task5, task6])
+        XCTAssertEqual(sut.tasksByDays(project)[0], [task1, task2, task3, task4, task5, task6])
 
-        try sut.addRelationship(relationship1)
-        try sut.addRelationship(relationship2)
-        let list = sut.tasksByDays()
+        try sut.addRelationship(&project, relationship1)
+        try sut.addRelationship(&project, relationship2)
+        let list = sut.tasksByDays(project)
         XCTAssertEqual(list[0], [task1, task4, task5, task6])
         XCTAssertEqual(list[9], [task2])
         XCTAssertEqual(list[14], [task3])
     }
 
     func testCycleReference() throws {
-        var sut = createSUT()
+        let sut = createSUT()
+        var project = createProject()
         let task1 = createTask(id: .init(1))
         let task2 = createTask(id: .init(2))
         let task3 = createTask(id: .init(3))
         let task4 = createTask(id: .init(4))
 
-        try sut.addTask(task1)
-        try sut.addTask(task2)
-        try sut.addTask(task3)
-        try sut.addTask(task4)
+        try sut.addTask(&project, task1)
+        try sut.addTask(&project, task2)
+        try sut.addTask(&project, task3)
+        try sut.addTask(&project, task4)
 
         let relationship1 = Relationship(id: .init(1),
                                         influencer: task1,
@@ -161,11 +167,11 @@ class modelsTests: XCTestCase {
                                         dependant: task1,
                                         daysGap: 0)
 
-        try sut.addRelationship(relationship1)
-        try sut.addRelationship(relationship2)
+        try sut.addRelationship(&project, relationship1)
+        try sut.addRelationship(&project, relationship2)
 
         // task2 depends on task1, task3 depends on task2, task1 can't depend on task3
-        XCTAssertThrowsError(try sut.addRelationship(relationship3)) { error in
+        XCTAssertThrowsError(try sut.addRelationship(&project, relationship3)) { error in
             XCTAssertEqual(error as? MEditingProjectError, .cycleReference)
         }
 
@@ -175,15 +181,19 @@ class modelsTests: XCTestCase {
                                         daysGap: 0)
 
         // task3 already depends on task1 through task2, so no need to add this relationship
-        XCTAssertThrowsError(try sut.addRelationship(relationship4)) { error in
+        XCTAssertThrowsError(try sut.addRelationship(&project, relationship4)) { error in
             XCTAssertEqual(error as? MEditingProjectError, .taskAlreadyDependsOnInfluencerIndirectly)
         }
     }
 
     // MARK: Private
-    private func createSUT(id: Int = 1,
-                           name: String = "Morros de Alihuen",
-                           startDate: Date = Date()) -> MProject {
+    private func createSUT() -> MProjectHelperProtocol {
+        return MProjectHelper()
+    }
+
+    private func createProject(id: Int = 1,
+                               name: String = "Morros de Alihuen",
+                               startDate: Date = Date()) -> MProject {
         let project = MProject(id: id,
                                name: name,
                                startDate: startDate)
